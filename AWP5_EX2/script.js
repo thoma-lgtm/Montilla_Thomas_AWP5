7d286e70cffb78b4529bf851c5c10c2a00edbbba
Thoma <thomasdejesus6859@iessineu.net>
2026-05-25 10:38:17 +0200

    ya funciona el 2

--- AWP5_EX2/script.js
+++ AWP5_EX2/script.js
@@ -1,35 +1,32 @@
-const currencyEl_one = document.getElementById
-('currency-one');
-const amountEl_two = document.getElementById
-('amount-two');
-const currencyEl_two = document.getElementById
-('currency-two');
-const amountEl_two = document.getElementById
-('amount-two');
+const currencyEl_one = document.getElementById('currency_one');
+const amountEl_one = document.getElementById('amount_one');
 
-const rateEl = document.getElementById('rate')
-const swap = document.getElementById('swap')
+const currencyEl_two = document.getElementById('currency_two');
+const amountEl_two = document.getElementById('amount_two');
 
-//Cambio de moneda extanjera?
-function calculate(){
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Cambio de moneda extranjera
+function calculate() {
     const currency_one = currencyEl_one.value;
     const currency_two = currencyEl_two.value;
 
-    fetch (` https://v6.exchangerate-api.com/v6/9711be5a77600ddbe4da450b/latest/USD${currency_one}`)
-    .then(res => res.json())
-    .then(data => {
-        //Console.log(data)
-        const rate = data.rates[currency_two];
+    fetch(`https://v6.exchangerate-api.com/v6/9711be5a77600ddbe4da450b/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.conversion_rates[currency_two];
 
-        rateEl.innerText = `1 ${currency_one} = ${rate} ${currencyEl_two}`;
+            rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
-    });
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        });
 }
 
 // Lista de eventos
-currencyEl_one.addEventListener('change', calculate)
-amountEl_one.addEventListener('input', calculate)
-currencyEl_two.addEventListener('change', calculate)
-amountEl_two.addEventListener('input', calculate)
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
 
 calculate();
