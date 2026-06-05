cedeff503707b0747202c53d17da2d7ccc2341ad
Thoma <thomasdejesus6859@iessineu.net>
2026-06-05 10:06:49 +0200

    FUNCIONA

--- AWP5_EX3/script.js
+++ AWP5_EX3/script.js
@@ -5,35 +5,40 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie')
 const moneySelect = document.getElementById('money');
 
-
-populateUI();
-
 let ticketPrice = +movieSelect.value;
 let currentCurrency = moneySelect.value; //Marca como base el dolar al momento de cambiar
 let exchangeRate = 1;
 
+populateUI();
 
 //
 function setMovieData(movieIndex, moviePrice){
     localStorage.setItem('selectedMovieIndex', movieIndex);
-    localStorage.setItem('selectedMoviePrice', moviePrice)
+    localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
 //Actualizador de precios?
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-    const seatsIndex = [...selectedSeats].map (seat => [...seats].indexOf(seat));
-
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
-    const convertedTotal = (
-        selectedSeatsCount *
-        ticketPrice *
-        exchangeRate
-    ).toFixed(2);
+    const convertedTotal = (selectedSeatsCount * ticketPrice * exchangeRate).toFixed(2);
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = `${convertedTotal} ${currentCurrency}`;
+}
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map (seat => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+    const convertedTotal = (selectedSeatsCount * ticketPrice * exchangeRate).toFixed(2);
 
     count.innerText = selectedSeatsCount;
     total.innerText = `${convertedTotal} ${currentCurrency}`;
@@ -41,8 +46,7 @@ function updateSelectedCount(){
 
 //populate UI
 function populateUI(){
-    const selectedSeats = JSON.parse(
-        localStorage.getItem('selectedSeats'));
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
 
     if(selectedSeats !== null && selectedSeats.length > 0) {
         seats.forEach(( seat, index) => {
@@ -56,10 +60,10 @@ function populateUI(){
 
     if(selectedMovieIndex !== null) {
         movieSelect.selectedIndex = selectedMovieIndex;
+        ticketPrice = +movieSelect.value;
     }
 
-     const selectedCurrency =
-        localStorage.getItem('selectedCurrency');
+     const selectedCurrency = localStorage.getItem('selectedCurrency');
 
     if (selectedCurrency) {
         moneySelect.value = selectedCurrency;
@@ -81,7 +85,6 @@ container.addEventListener('click', e => {
         !e.target.classList.contains('occupied')
     ) {
         e.target.classList.toggle('selected');
-
         updateSelectedCount();
     }
 });
