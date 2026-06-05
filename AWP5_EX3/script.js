27e7dfe694c2db9ba02d0634ca4acb7fc7005e29
Thoma <thomasdejesus6859@iessineu.net>
2026-06-05 08:18:55 +0200

    No funcionaa,

--- AWP5_EX3/script.js
+++ AWP5_EX3/script.js
@@ -3,10 +3,15 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie')
+const moneySelect = document.getElementById('money');
+
 
 populateUI();
 
 let ticketPrice = +movieSelect.value;
+let currentCurrency = moneySelect.value; //Marca como base el dolar al momento de cambiar
+let exchangeRate = 1;
+
 
 //
 function setMovieData(movieIndex, moviePrice){
@@ -24,13 +29,20 @@ function updateSelectedCount(){
 
     const selectedSeatsCount = selectedSeats.length;
 
+    const convertedTotal = (
+        selectedSeatsCount *
+        ticketPrice *
+        exchangeRate
+    ).toFixed(2);
+
     count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
+    total.innerText = `${convertedTotal} ${currentCurrency}`;
 }
 
 //populate UI
 function populateUI(){
-    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    const selectedSeats = JSON.parse(
+        localStorage.getItem('selectedSeats'));
 
     if(selectedSeats !== null && selectedSeats.length > 0) {
         seats.forEach(( seat, index) => {
@@ -44,6 +56,14 @@ function populateUI(){
 
     if(selectedMovieIndex !== null) {
         movieSelect.selectedIndex = selectedMovieIndex;
+    }
+
+     const selectedCurrency =
+        localStorage.getItem('selectedCurrency');
+
+    if (selectedCurrency) {
+        moneySelect.value = selectedCurrency;
+        currentCurrency = selectedCurrency;
     }
 }
 
@@ -68,9 +88,34 @@ container.addEventListener('click', e => {
 
 //moneyyyy 🤑
 
-const moneySelect = document.getElementById('money');
+function getExchangeRate() {
+    currentCurrency = moneySelect.value;
+
+    fetch('https://api.exchangerate-api.com/v4/latest/USD')
+        .then(res => res.json())
+        .then(data => {
+            if (!data.rates[currentCurrency]) {
+                console.error('Moneda no disponible');
+                return;
+            }
+
+            exchangeRate = data.rates[currentCurrency];
+
+            localStorage.setItem('selectedCurrency', currentCurrency);
+
+            updateSelectedCount();
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+moneySelect.addEventListener(
+    'change',
+    getExchangeRate
+);
 
 
 
 //Total set
-updateSelectedCount();
+getExchangeRate();
